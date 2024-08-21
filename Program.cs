78bb376721054381152698895eed360a3f516e9f
Daniel.botelho <daniel.monteiro8991@gmail.com>
2024-08-20 21:51:28 -0300

    at

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
-﻿using c_.models;
+﻿using DIO_XP.models;
 
 Pessoa p = new Pessoa();
+
+p.Nome = "Daniel";
+p.Idade = 26;
+p.Apresentar();
+
--- models/Pessoa.cs
+++ models/Pessoa.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace c_.models
+namespace DIO_XP.models
 {
     public class Pessoa
     {
         public string Nome { get; set; }
         public int Idade { get; set; }
 
-        public void apresentar()
+        public void Apresentar()
         {
             Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos.");
         }

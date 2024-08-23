4c79411696c3f249ad572d8c8b44850bc2049bb4
Daniel.botelho <daniel.monteiro8991@gmail.com>
2024-08-22 23:28:02 -0300

    att

--- obj/Debug/net8.0/c#.AssemblyInfo.cs
+++ obj/Debug/net8.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+117c7da44e130a9b944bf43d9793017c8ed63ee3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8e2fad7dd4ab5457da628d6f877264093b43363")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
